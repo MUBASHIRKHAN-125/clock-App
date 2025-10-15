cf8a840dfc2730c41833ecf69673c1cba0735a51
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-10-15 17:18:22 +0500

    done-clock-app

---
+++ clock.js
@@ -0,0 +1,34 @@
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+
+const updateTime = () => {
+
+    setInterval(() => {
+
+        setInterval(() => {
+
+            const currentDate = new Date();
+
+            let hr = currentDate.getHours();
+            let min = currentDate.getMinutes();
+            let sec = currentDate.getSeconds();
+
+            let session = hr >= 12 ? "PM" : "AM";
+            hr = hr % 12 || 12;
+
+            hr = hr < 10 ? "0" + hr : hr;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+
+            hours.innerText = hr;
+            minutes.innerText = min;
+            seconds.innerText = sec;
+
+            document.getElementById("session").innerText = session;
+
+        }, 1000);
+
+    });
+};
+updateTime();
